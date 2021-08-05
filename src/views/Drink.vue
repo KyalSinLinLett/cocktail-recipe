a862cbad9633041890e89b83b047099de8c6d857
KyalSinLinLett <alexsin0012@gmail.com>
2021-08-05 13:24:40 +0630

    ingredients page

--- src/views/Drink.vue
+++ src/views/Drink.vue
@@ -2,7 +2,10 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Random Recipe</ion-title>
+        <ion-buttons slot="start">
+          <ion-back-button>Back</ion-back-button>
+        </ion-buttons>
+        <ion-title size="medium">Drink</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content v-if="state.loading">
@@ -10,35 +13,35 @@
         <ion-spinner color="primary"></ion-spinner>
       </div>
     </ion-content>
+    <ion-item v-if="state.error">
+      <ion-label>Drink not found...</ion-label>
+    </ion-item>
     <ion-content :fullscreen="true" v-else>
-      <ion-refresher slot="fixed" @ionRefresh="doRefresh">
-        <ion-refresher-content> </ion-refresher-content>
-      </ion-refresher>
       <ion-card>
-        <img :src="state.randomCocktail.strDrinkThumb" />
+        <img :src="state.cocktail.strDrinkThumb" />
         <ion-card-header>
           <ion-card-subtitle>
-            {{ state.randomCocktail.strCategory }} | Served in
-            {{ state.randomCocktail.strGlass }}
+            {{ state.cocktail.strCategory }} | Served in
+            {{ state.cocktail.strGlass }}
           </ion-card-subtitle>
           <ion-card-title>
-            {{ state.randomCocktail.strDrink }}
+            {{ state.cocktail.strDrink }}
           </ion-card-title>
         </ion-card-header>
         <ion-card-content>
-          <p>{{ state.randomCocktail.strInstructions }}</p>
+          <p>{{ state.cocktail.strInstructions }}</p>
           <ion-list-header>Ingredients</ion-list-header>
           <ion-list>
             <ion-item
               v-for="num in 15"
               :key="num"
-              v-show="state.randomCocktail['strIngredient' + num]"
+              v-show="state.cocktail['strIngredient' + num]"
             >
               <ion-label>
-                <span v-if="state.randomCocktail['strMeasure' + num]">
-                  {{ state.randomCocktail["strMeasure" + num] }} -
+                <span v-if="state.cocktail['strMeasure' + num]">
+                  {{ state.cocktail["strMeasure" + num] }} -
                 </span>
-                {{ state.randomCocktail["strIngredient" + num] }}
+                {{ state.cocktail["strIngredient" + num] }}
               </ion-label>
             </ion-item>
           </ion-list>
@@ -50,6 +53,7 @@
 
 <script lang="ts">
 import { reactive } from "vue";
+import { useRoute } from "vue-router";
 import axios from "axios";
 import { ServerResponse } from "@/model/cocktail";
 
@@ -60,8 +64,6 @@ import {
   IonTitle,
   IonContent,
   IonSpinner,
-  IonRefresher,
-  IonRefresherContent,
   IonCard,
   IonCardSubtitle,
   IonCardContent,
@@ -71,6 +73,8 @@ import {
   IonLabel,
   IonCardTitle,
   IonCardHeader,
+  IonBackButton,
+  IonButtons,
 } from "@ionic/vue";
 
 export default {
@@ -82,8 +86,6 @@ export default {
     IonContent,
     IonPage,
     IonSpinner,
-    IonRefresher,
-    IonRefresherContent,
     IonItem,
     IonList,
     IonListHeader,
@@ -93,44 +95,56 @@ export default {
     IonCardSubtitle,
     IonCardContent,
     IonCardHeader,
+    IonBackButton,
+    IonButtons,
   },
 
   setup() {
     const state = reactive({
-      randomCocktail: {},
+      cocktail: {},
       loading: false,
+      error: false,
     });
 
-    const fetchRandomCocktail = async (displayLoaderPage: boolean) => {
+    const route = useRoute();
+
+    const drinkId = route.params.drinkId as string;
+
+    const fetchCocktail = async (displayLoaderPage: boolean) => {
       if (displayLoaderPage) {
         state.loading = true;
       }
 
       axios
         .request<string>({
-          url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+          url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`,
           transformResponse: (r: ServerResponse) => r,
         })
         .then((response) => {
           const data = { drinks: response.data };
           const drinks = JSON.parse(data.drinks).drinks[0];
 
-          state.randomCocktail = drinks;
+          state.cocktail = drinks;
           state.loading = false;
+          state.error = false;
+        })
+        .catch((_) => {
+          /* eslint-disable */
+          state.error = true;
         });
     };
 
     const doRefresh = (event: any) => {
-      fetchRandomCocktail(false);
+      fetchCocktail(false);
 
       event.target.complete();
     };
 
-    fetchRandomCocktail(true);
+    fetchCocktail(true);
 
     return {
       state,
-      fetchRandomCocktail,
+      fetchCocktail,
       doRefresh,
     };
   },
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -2,27 +2,128 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Search</ion-title>
       </ion-toolbar>
+      <ion-searchbar
+        debounce="500"
+        :onIonChange="(e) => fetchRecipe(e.detail.value)"
+        placeholder="i.e Vodka"
+      ></ion-searchbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+    <ion-content v-if="state.loading">
+      <div class="loading-center">
+        <ion-spinner color="primary"></ion-spinner>
+      </div>
+    </ion-content>
+    <ion-content :fullscreen="true" v-else>
+      <ion-item v-if="state.error">
+        <ion-label>Drink not found...</ion-label>
+      </ion-item>
+      <ion-card
+        v-show="!state.error"
+        v-for="rec in state.lstIngredients"
+        :key="rec"
+        @click="() => router.push(`/drink/${rec.idDrink}`)"
+      >
+        <img :src="rec.strDrinkThumb" />
+        <ion-card-header>
+          <ion-card-title>
+            {{ rec.strDrink }}
+          </ion-card-title>
+          <ion-card-subtitle>Tap for recipe...</ion-card-subtitle>
+        </ion-card-header>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { reactive } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSearchbar,
+  IonCard,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardHeader,
+  IonItem,
+  IonLabel,
+  IonSpinner,
+} from "@ionic/vue";
 
-export default  {
-  name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Tab2",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonSearchbar,
+    IonCard,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardHeader,
+    IonItem,
+    IonLabel,
+    IonSpinner,
+  },
+
+  setup() {
+    const router = useRouter();
+    const state = reactive({
+      lstIngredients: [],
+      loading: false,
+      error: false,
+    });
+
+    const fetchRecipe = async (searchTerm: string) => {
+      if (searchTerm) {
+        state.loading = true;
+
+        axios
+          .request<any>({
+            url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchTerm}`,
+            transformResponse: (r: any) => r,
+          })
+          .then((response) => {
+            const data = { drinks: response.data };
+            state.lstIngredients = JSON.parse(data.drinks).drinks;
+            state.loading = false;
+            state.error = false;
+          })
+          .catch((_) => {
+            /* eslint-disable */
+            state.error = true;
+            state.loading = false;
+          });
+      }
+    };
+
+    return {
+      state,
+      router,
+      fetchRecipe,
+    };
+  },
+};
 </script>
+
+<style scoped>
+.loading-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 90vh;
+}
+
+ion-spinner {
+  transform: scale(1.5);
+}
+</style>
